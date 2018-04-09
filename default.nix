3e468978718338abf036e669d8471f1ddf7c5141
Ollie Charles <ollie@ocharles.org.uk>
2018-04-09 17:44:26 +0100

    Re-generate default.nix

--- default.nix
+++ default.nix
@@ -1,16 +1,21 @@
-{ mkDerivation, base, one-liner, opaleye, postgresql-simple
-, profunctors, scientific, stdenv, streaming
-, tagged, text, exceptions, free, streaming-postgresql-simple
-, lens
+{ mkDerivation, adjunctions, aeson, base, bytestring, contravariant
+, exceptions, free, lens, monad-control, mtl, one-liner, opaleye
+, postgresql-simple, profunctors, resourcet, scientific, stdenv
+, streaming, streaming-postgresql-simple, tagged, text, time
+, transformers, uuid, vector
 }:
 mkDerivation {
   pname = "rel8";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    base one-liner opaleye postgresql-simple
-    profunctors scientific streaming tagged text exceptions free
-    streaming-postgresql-simple lens
+    adjunctions aeson base bytestring contravariant exceptions free
+    lens monad-control mtl one-liner opaleye postgresql-simple
+    profunctors resourcet scientific streaming
+    streaming-postgresql-simple tagged text time transformers uuid
+    vector
   ];
+  testHaskellDepends = [ base ];
+  description = "A type-safe, expressive and concise API for querying relational databases using Opaleye";
   license = stdenv.lib.licenses.bsd3;
 }
