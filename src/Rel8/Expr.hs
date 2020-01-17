dcc26675fcdacccdf7999a0a70a6a93aeab39241
Ollie Charles <ollie@ocharles.org.uk>
2020-01-17 16:30:41 +0000

    DBType for Expr to create literals

--- src/Rel8/Expr.hs
+++ src/Rel8/Expr.hs
@@ -7,7 +7,9 @@
 module Rel8.Expr where
 
 import Data.Kind
+import Rel8.Column
 import Rel8.Table
+import Rel8.ZipLeaves
 import qualified Opaleye.Internal.HaskellDB.PrimQuery as Opaleye
 
 
@@ -20,3 +22,20 @@ type role Expr representational nominal
 
 instance Table ( Expr m a ) where
   type ExprIn ( Expr m a ) = Expr m
+
+
+class DBType a where
+  lit :: a -> Expr m a
+
+
+instance DBType Bool where
+  lit =
+    Expr . Opaleye.ConstExpr . Opaleye.BoolLit
+
+
+instance a ~ b => ZipLeaves ( Expr m a ) ( Expr n b ) ( Expr m ) ( Expr n ) where
+  type CanZipLeaves ( Expr m a ) ( Expr n b ) c =
+    c a
+
+  zipLeaves _ f e1 e2 =
+    toColumn <$> f ( C e1 ) ( C e2 )
