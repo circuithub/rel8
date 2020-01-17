9e51f1ab08e07536c8b4a57a89b30dbbae42ad5e
Ollie Charles <ollie@ocharles.org.uk>
2020-01-17 19:37:41 +0000

    Document Column

--- src/Rel8/Column.hs
+++ src/Rel8/Column.hs
@@ -6,6 +6,10 @@ import Data.Functor.Identity
 import Data.Kind
 
 
+-- | The @Column@ type family should be used to indicate which fields of your
+-- data types are single columns in queries. This type family has special
+-- support when a query is executed, allowing you to use a single data type for
+-- both query data and rows decoded to Haskell.
 type family Column ( f :: Type -> Type ) ( a :: Type ) :: Type where
   Column Identity a = a
   Column f a = f a
