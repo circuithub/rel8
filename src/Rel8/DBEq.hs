8a7c803397d205f282e12d4769edd8903840e9a5
Ollie Charles <ollie@ocharles.org.uk>
2020-01-17 16:45:49 +0000

    Define DBTypeEq for String and Int

--- src/Rel8/DBEq.hs
+++ src/Rel8/DBEq.hs
@@ -3,16 +3,17 @@
 module Rel8.DBEq where
 
 import Rel8.Expr
+import qualified Opaleye.Internal.HaskellDB.PrimQuery as Opaleye
 
 
 -- | The class of database types that support equality.
 class DBTypeEq a where
   eqExprs :: Expr m a -> Expr m a -> Expr m Bool
+  eqExprs ( Expr a ) ( Expr b ) =
+    Expr ( Opaleye.BinExpr (Opaleye.:==) a b )
 
 
-instance DBTypeEq String where
-  eqExprs = undefined
+instance DBTypeEq String
 
 
-instance DBTypeEq Int where
-  eqExprs = undefined
+instance DBTypeEq Int
