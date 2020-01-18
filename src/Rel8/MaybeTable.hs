dd4f07aa4ea669946a4885667b9ca68dbc59e286
Oliver Charles <ollie@ocharles.org.uk>
2020-01-18 18:13:17 +0000

    Document MaybeTable

--- src/Rel8/MaybeTable.hs
+++ src/Rel8/MaybeTable.hs
@@ -12,6 +12,11 @@ module Rel8.MaybeTable ( MaybeTable(..) ) where
 import Rel8.Column
 
 
+{-| @MaybeTable t@ is the table @t@, but as the result of an outer join. If the
+outer join fails to match any rows, this is essentialy @Nothing@, and if the
+outer join does match rows, this is like @Just@.
+
+-}
 data MaybeTable t f =
   MaybeTable
     { isNull :: Column f Bool
