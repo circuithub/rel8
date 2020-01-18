009f8f12b693a78bb3864e3f34c33376d50abe67
Oliver Charles <ollie@ocharles.org.uk>
2020-01-18 17:28:11 +0000

    Document Nest

--- src/Rel8/Nest.hs
+++ src/Rel8/Nest.hs
@@ -3,5 +3,7 @@
 module Rel8.Nest where
 
 
+-- | @Nest m@ adds a new distinct scope to the monad @m@. Expressions produced
+-- in @m@ cannot be used in @Nest m@ (and vice versa).
 newtype Nest m a = Nest ( m a )
   deriving ( Functor, Applicative, Monad )
