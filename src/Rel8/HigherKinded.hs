e5f690a5905b4e8cd8873e90294ab5151e235fa7
Oliver Charles <ollie@ocharles.org.uk>
2020-01-18 11:46:04 +0000

    Document HigherKinded

--- src/Rel8/HigherKinded.hs
+++ src/Rel8/HigherKinded.hs
@@ -14,7 +14,52 @@ import Rel8.Column ( C )
 import Rel8.Top ( Top )
 
 
--- | The class of higher-kinded data types.
+{-| The class of higher-kinded data types.
+
+Higher-kinded data types are data types of the pattern:
+
+@
+data MyType f =
+  MyType { field1 :: Column f T1 OR HK1 f
+         , field2 :: Column f T2 OR HK2 f
+         , ...
+         , fieldN :: Column f Tn OR HKn f
+         }
+@
+
+where @Tn@ is any Haskell type, and @HKn@ is any higher-kinded type.
+
+That is, higher-kinded data are records where all fields in the record
+are all either of the type @Column f T@ (for any @T@), or are themselves
+higher-kinded data:
+
+[Nested]
+
+@
+data Nested f =
+  Nested { nested1 :: MyType f
+         , nested2 :: MyType f
+         }
+@
+
+The @HigherKinded@ type class is used to give us a special mapping operation
+that lets us change the type parameter @f@.
+
+[Supplying @HigherKinded@ instances]
+
+This type class should be derived generically for all table types in your
+project. To do this, enable the @DeriveAnyType@ and @DeriveGeneric@ language
+extensions:
+
+@
+\{\-\# LANGUAGE DeriveAnyClass, DeriveGeneric #-\}
+import qualified GHC.Generics
+
+data MyType f = MyType { fieldA :: Column f T }
+  deriving ( GHC.Generics.Generic, HigherKinded )
+@
+
+-}
 class HigherKinded t where
   type ZipRecord t (f :: * -> *) (g :: * -> *) (c :: * -> Constraint) :: Constraint
 
