318bff4ae1a78368de0db4a3b3f4147eda816f80
Oliver Charles <ollie@ocharles.org.uk>
2020-01-18 12:01:48 +0000

    Expand documentation for Column

--- src/Rel8/Column.hs
+++ src/Rel8/Column.hs
@@ -6,10 +6,38 @@ import Data.Functor.Identity
 import Data.Kind
 
 
--- | The @Column@ type family should be used to indicate which fields of your
--- data types are single columns in queries. This type family has special
--- support when a query is executed, allowing you to use a single data type for
--- both query data and rows decoded to Haskell.
+{-| The @Column@ type family should be used to indicate which fields of your
+data types are single columns in queries. This type family has special
+support when a query is executed, allowing you to use a single data type for
+both query data and rows decoded to Haskell.
+
+To understand why this type family is special, let's consider a simple
+higher-kinded data type of Haskell packages:
+
+@
+data HaskellPackage f =
+  HaskellPackage
+    { packageName :: Column f String
+    , packageAuthor :: Column f String
+    }
+@
+
+In queries, @f@ will be some type of 'Expr', and @Column ( Expr ..)@ reduces
+to just @Expr ..@:
+
+>>> :t packageName ( package :: Package ( Expr m ) )
+Expr m String
+
+When we 'select' queries of this type, @f@ will be instantiated as @Identity@,
+at which point all wrapping entire disappears:
+
+>>> :t packageName ( package :: Package Identity )
+String
+
+In @rel8@ we try hard to always know roughly what @f@ is, which means typed
+holes should mention precise types, rather than the @Column@ type family. You
+should only need to be aware of the type family when defining your table types.
+-}
 type family Column ( f :: Type -> Type ) ( a :: Type ) :: Type where
   Column Identity a = a
   Column f a = f a
