2204dad872b09e98eade57e4af06f5b4907b20ba
Oliver Charles <ollie@ocharles.org.uk>
2020-01-20 10:49:39 +0000

    Remove support for NULL for now, need to think about this carefully

--- src/Rel8/Expr.hs
+++ src/Rel8/Expr.hs
@@ -34,11 +34,6 @@ instance ( IsString a, DBType a ) => IsString ( Expr m a ) where
     lit . fromString
 
 
-instance {-# overlaps #-} ( IsString a, DBType a ) => IsString ( Expr m ( Maybe a ) ) where
-  fromString =
-    lit . Just . fromString
-
-
 {-| Haskell types that can be represented as expressiosn in a database. There
 should be an instance of @DBType@ for all column types in your database
 schema (e.g., @int@, @timestamptz@, etc).
@@ -79,11 +74,6 @@ class DBType ( a :: Type ) where
 
   default lit :: Show a => a -> Expr m a
   lit = unsafeCoerceExpr . lit . show
-
-
-instance DBType a => DBType ( Maybe a ) where
-  lit =
-    maybe ( Expr ( Opaleye.ConstExpr Opaleye.NullLit ) ) ( unsafeCoerceExpr .  lit )
 
 
 -- | Corresponds to the @bool@ PostgreSQL type.
