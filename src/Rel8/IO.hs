9bc9b9118c11467c13ef85a8b9da954601956bc7
Oliver Charles <ollie@ocharles.org.uk>
2020-04-13 19:44:28 +0100

    Implemented each

--- src/Rel8/IO.hs
+++ src/Rel8/IO.hs
@@ -10,9 +10,7 @@ import Control.Monad ( void )
 import Data.Indexed.Functor.Traversable ( HTraversable(..) )
 import Data.Monoid ( Any(..) )
 import Database.PostgreSQL.Simple ( Connection )
-import qualified Opaleye.Internal.PackMap as Opaleye
 import qualified Opaleye.Internal.RunQuery as Opaleye
-import qualified Opaleye.Internal.Unpackspec as Opaleye
 import qualified Opaleye.RunQuery as Opaleye
 import Rel8.Expr
 import Rel8.Query
@@ -24,11 +22,7 @@ select c = Opaleye.runQueryExplicit queryRunner c . toOpaleye
 
 
 queryRunner :: Table a  => Opaleye.QueryRunner (Expr a) a
-queryRunner = Opaleye.QueryRunner unpackspec (const rowParser) hasColumns
-
-
-unpackspec :: HTraversable (Pattern a) => Opaleye.Unpackspec (Expr a) ()
-unpackspec = Opaleye.Unpackspec $ Opaleye.PackMap \f (Expr x) -> void $ htraverse (\expr -> expr <$ f (getConst expr)) x
+queryRunner = Opaleye.QueryRunner (void unpackspec) (const rowParser) hasColumns
 
 
 hasColumns :: HTraversable (Pattern a) => Expr a -> Bool
--- src/Rel8/Query.hs
+++ src/Rel8/Query.hs
@@ -1,20 +1,32 @@
 {-# language BlockArguments #-}
 {-# language DerivingVia #-}
+{-# language FlexibleContexts #-}
 {-# language GeneralizedNewtypeDeriving #-}
+{-# language TypeApplications #-}
 
 module Rel8.Query where
 
+import Control.Applicative ( Const(..) )
 import Control.Arrow ( Arrow, ArrowChoice, Kleisli(..) )
 import Control.Category ( Category )
-import Control.Monad.Trans.State.Strict ( State, runState )
+import Control.Monad.Trans.State.Strict ( State, runState, state )
 import Data.Coerce
+import Data.Indexed.Functor.Representable ( HRepresentable(..) )
+import Data.Indexed.Functor.Traversable ( HTraversable(..) )
 import Data.Profunctor ( Profunctor, Strong, Choice, Star(..) )
+import Data.Profunctor ( lmap )
 import Data.Profunctor.Traversing ( Traversing )
+import qualified Opaleye.Internal.HaskellDB.PrimQuery as Opaleye
+import qualified Opaleye.Internal.PackMap as Opaleye
 import qualified Opaleye.Internal.PrimQuery as Opaleye
 import qualified Opaleye.Internal.QueryArr as Opaleye
+import qualified Opaleye.Internal.Table as Opaleye
 import qualified Opaleye.Internal.Tag as Opaleye
+import qualified Opaleye.Internal.Unpackspec as Opaleye
+import qualified Opaleye.Table as Opaleye
 import Rel8.Expr
 import Rel8.Schema
+import Rel8.Table
 
 
 newtype Query a b =
@@ -27,8 +39,19 @@ runQuery :: a -> Query a b -> (b, QueryState)
 runQuery a q = runState (coerce q a) emptyQueryState
 
 
-each :: Schema a -> Query x (Expr a)
-each = undefined
+each :: Table a => Schema a -> Query x (Expr a)
+each (Schema columnNames) =
+  lmap (const ()) $
+  fromOpaleye $
+  Opaleye.selectTableExplicit unpackspec $
+  Opaleye.Table "foo" $
+  Opaleye.TableProperties
+    (Opaleye.Writer @() $ Opaleye.PackMap \_ _ -> pure ())
+    (Opaleye.View $ Expr $ htabulate \i -> Const $ Opaleye.BaseTableAttrExpr $ getConst $ hindex columnNames i)
+
+
+unpackspec :: HTraversable (Pattern a) => Opaleye.Unpackspec (Expr a) (Expr a)
+unpackspec = Opaleye.Unpackspec $ Opaleye.PackMap \f (Expr x) -> fmap Expr $ htraverse (fmap Const . f . getConst) x
 
 
 optional :: Query a (Expr b) -> Query a (Expr (Maybe b))
@@ -60,3 +83,10 @@ toOpaleye (Query (Star m)) =
   Opaleye.QueryArr \(a, pq, t0) -> out (runState (m a) (QueryState pq t0))
   where
     out (b, QueryState pq t) = (b, pq, t)
+
+
+fromOpaleye :: Opaleye.QueryArr a b -> Query a b
+fromOpaleye (Opaleye.QueryArr f) =
+  Query $ Star $ \a -> state \(QueryState pq t) -> out (f (a, pq, t))
+  where
+    out (b, pq, t) = (b, QueryState pq t)
