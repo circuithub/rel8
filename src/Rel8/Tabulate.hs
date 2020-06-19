2e25bc6e901503b83fc84d57675ecb924dcc5e83
Shane <me@shane.sh>
2020-06-19 17:41:20 +0100

    Rel8.Tabulate without i

--- src/Rel8/Tabulate.hs
+++ src/Rel8/Tabulate.hs
@@ -39,56 +39,17 @@ where
 
 -- base
 import Control.Applicative ( liftA2 )
-import Control.Arrow
-  ( Arrow
-  , ArrowApply
-  , ArrowChoice
-  , Kleisli(..)
-  , (&&&)
-  , app
-  , arr
-  , returnA
-  , second
-  )
-import qualified Control.Arrow as A
-import Control.Category ( Category, (.), id )
-import Control.Monad ( join )
-import Data.Bifunctor ( bimap )
-import Prelude hiding ( (.), filter, id, lookup, zip, zipWith )
-
--- opaleye
-import Opaleye.Internal.Lateral ( bilaterally, lateral, laterally )
-
--- product-profunctors
-import Data.Profunctor.Product ( ProductProfunctor( (****), purePP ) )
-
--- profunctors
-import Data.Profunctor
-  ( Choice
-  , Profunctor
-  , Strong
-  , dimap
-  , first'
-  , left'
-  , lmap
-  , right'
-  , second'
-  )
+import Control.Monad ( (>=>), join )
+import Prelude hiding ( filter, lookup, zip, zipWith )
 
 -- rel8
 import Rel8
-  ( Context
-  , DBEq
-  , Expr
+  ( Expr
   , MaybeTable
   , EqTable
   , Query
-  , Table
-  , (&&.)
   , (==.)
   , filter
-  , lit
-  , not_
   , optional
   , where_
   )
@@ -97,7 +58,6 @@ import Rel8
 import Data.Functor.Apply ( Apply, liftF2 )
 import Data.Functor.Bind ( Bind )
 import qualified Data.Functor.Bind
-import Data.Semigroupoid ( Semigroupoid, o )
 
 
 -- | To understand what a @'Tabulation' k i a@ is, consider first a
@@ -113,80 +73,30 @@ import Data.Semigroupoid ( Semigroupoid, o )
 -- \"Identity\" 'Tabulation's are created using 'tabulate'. 'Tabulation's can
 -- be 'precompose'd or 'postcompose'd with 'QueryArr's to form new
 -- 'Tabulation's.
-newtype Tabulation k i a = Tabulation (Query k -> Kleisli Query i (k, a))
+newtype Tabulation k a = Tabulation (Query k -> Query (k, a))
   deriving Functor
 
 
-instance Profunctor (Tabulation k) where
-  dimap f g (Tabulation tabulation) =
-    Tabulation $ dimap f (fmap g) . tabulation
-
-
-instance Strong (Tabulation k) where
-  second' (Tabulation f) = Tabulation $ fmap twist . second . f
-    where
-      twist (c, (k, b)) = (k, (c, b))
-
-
-instance Choice (Tabulation k) where
---   right' (Tabulation f) = Tabulation $ \ks ->
---     lateral (either (\c -> fmap (, Left c) ks) (pure . fmap Right))
---       . A.right (f ks)
-
-
-instance EqTable k => ProductProfunctor (Tabulation k) where
-  purePP = pure
-  (****) = (<*>)
-
-
-instance EqTable k => Semigroupoid (Tabulation k) where
-  o = (.)
-
-
-instance EqTable k => Category (Tabulation k) where
-  id = Tabulation $ \ks -> Kleisli \i -> do
-    k <- ks
-    return (k, i)
-  Tabulation f . Tabulation g = Tabulation $ \ks -> Kleisli \i -> do
-    (k, a) <- runKleisli (g ks) i
-    (l, b) <- runKleisli (f ks) a
-    where_ $ k ==. l
-    return (k, b)
-
-
-instance EqTable k => Arrow (Tabulation k) where
-  arr f = fmap f id
-  first = first'
-
-
-instance EqTable k => ArrowChoice (Tabulation k) where
-  left = left'
-
-
-instance EqTable k => ArrowApply (Tabulation k) where
-  app = Tabulation $ \ks -> lmap (bimap (\(Tabulation query) -> query ks) id) app
-
-
-instance EqTable k => Apply (Tabulation k i) where
+instance EqTable k => Apply (Tabulation k) where
   liftF2 = liftA2
 
 
-instance EqTable k => Applicative (Tabulation k i) where
-  pure a = Tabulation $ \ks -> Kleisli \_ -> do
+instance EqTable k => Applicative (Tabulation k) where
+  pure a = Tabulation $ \ks -> do
     k <- ks
     return (k, a)
   liftA2 = zipWith
 
 
-instance EqTable k => Bind (Tabulation k i) where
+instance EqTable k => Bind (Tabulation k) where
   join = join
 
 
-instance EqTable k => Monad (Tabulation k i) where
+instance EqTable k => Monad (Tabulation k) where
   Tabulation query >>= f = Tabulation \ks -> do
     (k, a) <- query ks
     case f a of
-      Tabulation query' -> Kleisli (filter ((k ==.) . fst)) . query' ks
+      Tabulation query' -> query' ks >>= filter ((k ==.) . fst)
 
 
 -- | 'tabulate' creates an \"identity\" @'Tabulation' k a a@ that allows @a@
@@ -212,23 +122,28 @@ instance EqTable k => Monad (Tabulation k i) where
 --   revisionsByProjectId :: 'Tabulation' ('Expr' ProjectId) (Revision 'Expr') (Revision 'Expr')
 --   revisionsByProjectId = 'tabulate' revisionProjectId
 --   @
-tabulate :: (a -> Query k) -> Tabulation k a a
-tabulate key = fromQuery \a -> (,a) <$> key a
+tabulate :: (a -> k) -> a -> Tabulation k a
+tabulate key = tabulateA (pure . key)
+
+
+tabulateA :: (a -> Query k) -> a -> Tabulation k a
+tabulateA key a = fromQuery $ (,a) <$> key a
 
 
 -- | Analgous to 'Data.Map.Strict.fromList'.
-fromQuery :: (i -> Query (k, a)) -> Tabulation k i a
-fromQuery = Tabulation . const . Kleisli
+fromQuery :: Query (k, a) -> Tabulation k a
+fromQuery = Tabulation . const
 
 
-indexed :: Tabulation k i a -> Tabulation k i (k, a)
+indexed :: Tabulation k a -> Tabulation k (k, a)
 indexed (Tabulation tabulation) = Tabulation $ fmap (liftA2 (,) fst id) . tabulation
 
 
-ifilter :: (k -> a -> Expr Bool) -> Tabulation k i a -> Tabulation k i a
-ifilter f tabulation = snd <$> filter (uncurry f) `postcompose` indexed tabulation
+ifilter :: (k -> a -> Expr Bool) -> Tabulation k a -> Tabulation k a
+ifilter f tabulation = snd <$> filter (uncurry f) `postbind` indexed tabulation
 
 
+{-
 -- | 'with' is like 'similarity', but for 'QueryArr's rather than
 -- 'Tabulation's. @'with' barsFromFoo . allFoos@ will get you all @Foo@s that
 -- have @Bar@s associated with them.
@@ -273,6 +188,7 @@ ifilter f tabulation = snd <$> filter (uncurry f) `postcompose` indexed tabulati
 -- 'QueryArr', if would it have returned any rows.
 -- exists :: (a -> Query b) -> a -> Query (Expr Bool)
 -- exists = fmap (not_ . isTableNull) . optional . with
+-}
 
 
 -- | Map a 'QueryArr' over the input side of a 'Tabulation'. In particular,
@@ -283,24 +199,24 @@ ifilter f tabulation = snd <$> filter (uncurry f) `postcompose` indexed tabulati
 -- @
 -- tabulation `'precompose'` 'queryTable'
 -- @
-precompose :: Tabulation k i a -> (j -> Query i) -> Tabulation k j a
-precompose (Tabulation f) g = Tabulation $ (. Kleisli g) . f
+prebind :: (i -> Tabulation k a) -> Query i -> Tabulation k a
+prebind f g = Tabulation $ \ks -> g >>= \i -> let Tabulation query = f i in query ks
 
 
-infixr 8 `precompose`
+infixr 8 `prebind`
 
 
 -- | Map a 'QueryArr' over the output side of a 'Tabulation'.
-postcompose :: (a -> Query b) -> Tabulation k i a -> Tabulation k i b
-postcompose f (Tabulation g) = Tabulation $ (second (Kleisli f) .) . g
+postbind :: (a -> Query b) -> Tabulation k a -> Tabulation k b
+postbind f g = let Tabulation query = g in Tabulation $ query >=> \(k, a) -> fmap (k ,) (f a)
 
 
-infixr 8 `postcompose`
+infixr 8 `postbind`
 
 
 -- | Note that because 'Tabulation' is a @MultiMap@, the 'Query' returned by
 -- 'lookup' can and often does contain multiple results.
-lookup :: EqTable k => k -> Tabulation k i a -> i -> Query a
+lookup :: EqTable k => k -> Tabulation k a -> Query a
 lookup = lookupAny . pure
 
 
@@ -313,9 +229,9 @@ lookup = lookupAny . pure
 -- | 'lookupAny' is like 'lookup', but instead of taking a single @k@, it
 -- takes a 'Query' of @k@s, and the resulting query will include @a@s that
 -- 'match'ed /any/ of the given @k@s. This is often used with 'Rel8.values'.
-lookupAny :: EqTable k => Query k -> Tabulation k i a -> i -> Query a
-lookupAny ks (Tabulation tabulation) i = do
-  (k, a) <- runKleisli (tabulation ks) i
+lookupAny :: EqTable k => Query k -> Tabulation k a -> Query a
+lookupAny ks (Tabulation tabulation) = do
+  (k, a) <- tabulation ks
   l <- ks
   where_ $ k ==. l
   return a
@@ -350,7 +266,7 @@ lookupAny ks (Tabulation tabulation) i = do
 --
 -- Analagous to [@rpadZip@](https://hackage.haskell.org/package/semialign/docs/Data-Semialign.html#v:rpadZip).
 leftAlign :: EqTable k
-  => Tabulation k i a -> Tabulation k i b -> Tabulation k i (a, MaybeTable b)
+  => Tabulation k a -> Tabulation k b -> Tabulation k (a, MaybeTable b)
 leftAlign = leftAlignWith (,)
 
 
@@ -359,10 +275,10 @@ leftAlign = leftAlignWith (,)
 -- Analagous to [@rpadZipWith@](https://hackage.haskell.org/package/semialign/docs/Data-Semialign.html#v:rpadZipWith).
 leftAlignWith :: EqTable k
   => (a -> MaybeTable b -> c)
-  -> Tabulation k i a -> Tabulation k i b -> Tabulation k i c
-leftAlignWith f (Tabulation left) (Tabulation right) = Tabulation $ \ks -> Kleisli \i -> do
-  (k, a) <- runKleisli (left ks) i
-  mlb <- optional $ filter ((k ==.) . fst) =<< runKleisli (right ks) i
+  -> Tabulation k a -> Tabulation k b -> Tabulation k c
+leftAlignWith f (Tabulation left) (Tabulation right) = Tabulation $ \ks -> do
+  (k, a) <- left ks
+  mlb <- optional $ filter ((k ==.) . fst) =<< right ks
   return (k, f a (snd <$> mlb))
 
 
@@ -386,21 +302,22 @@ leftAlignWith f (Tabulation left) (Tabulation right) = Tabulation $ \ks -> Kleis
 -- in terms of the number of keys, but \(\sum_{k} n_k \times m_k\) in terms
 -- of the number of values.
 zip :: EqTable k
-  => Tabulation k i a -> Tabulation k i b -> Tabulation k i (a, b)
+  => Tabulation k a -> Tabulation k b -> Tabulation k (a, b)
 zip = zipWith (,)
 
 
 -- | Analagous to [@zipWith@](https://hackage.haskell.org/package/semialign/docs/Data-Semialign.html#v:zipWith).
 -- See 'zip'.
 zipWith :: EqTable k
-  => (a -> b -> c) -> Tabulation k i a -> Tabulation k i b -> Tabulation k i c
-zipWith f (Tabulation left) (Tabulation right) = Tabulation \ks -> Kleisli \i -> do
-  (k, a) <- runKleisli (left ks) i
-  (l, b) <- runKleisli (right ks) i
+  => (a -> b -> c) -> Tabulation k a -> Tabulation k b -> Tabulation k c
+zipWith f (Tabulation left) (Tabulation right) = Tabulation \ks -> do
+  (k, a) <- left ks
+  (l, b) <- right ks
   where_ $ k ==. l
   return (k, f a b)
 
 
+{-
 -- -- | 'similarity' returns all the entries in the left 'Tabulation' that have
 -- -- a corresponding entry in the right 'Tabulation'. This corresponds to a
 -- -- semijoin in relational algebra.
@@ -432,3 +349,4 @@ zipWith f (Tabulation left) (Tabulation right) = Tabulation \ks -> Kleisli \i ->
 --   => (a -> b) -> Tabulation k i a -> Tabulation k i c
 -- aggregateTabulation agg (Tabulation query) =
 --   Tabulation (laterally aggregate . fmap (bimap groupKey agg) . query)
+-}
