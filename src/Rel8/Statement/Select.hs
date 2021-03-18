a779ca8346234f9f389c3616f08f89cc2ede468f
Oliver Charles <ollie@ocharles.org.uk>
2021-03-18 09:33:03 +0000

    Add missing files

---
+++ src/Rel8/Statement/Select.hs
@@ -0,0 +1,61 @@
+{-# language BlockArguments #-}
+{-# language FlexibleContexts #-}
+{-# language ScopedTypeVariables #-}
+{-# language TypeApplications #-}
+
+module Rel8.Statement.Select ( selectQuery, showQuery, select ) where
+
+-- base
+import Control.Exception ( throwIO )
+import Control.Monad.IO.Class ( MonadIO( liftIO ) )
+import Data.Foldable ( fold )
+
+-- hasql
+import Hasql.Connection ( Connection )
+import qualified Hasql.Decoders as Hasql
+import qualified Hasql.Encoders as Hasql
+import qualified Hasql.Session as Hasql
+import qualified Hasql.Statement as Hasql
+
+-- rel8
+import qualified Opaleye.Internal.Optimize as Opaleye
+import qualified Opaleye.Internal.Print as Opaleye ( formatAndShowSQL )
+import qualified Opaleye.Internal.QueryArr as Opaleye
+import Rel8.Expr ( Expr )
+import qualified Rel8.Optimize
+import Rel8.Query ( Query( Query ) )
+import Rel8.Serializable ( Serializable, hasqlRowDecoder )
+import Rel8.Table ( Table )
+import Rel8.Table.Opaleye ( unpackspec )
+
+-- text
+import Data.Text ( pack )
+import Data.Text.Encoding ( encodeUtf8 )
+
+
+-- | Run a @SELECT@ query, returning all rows.
+select :: forall row haskell m. (Serializable row haskell, MonadIO m) => Connection -> Query row -> m [haskell]
+select conn query = liftIO case selectQuery query of
+  Nothing -> return []
+  Just neQuery ->
+    Hasql.run session conn >>= either throwIO return
+    where
+      session = Hasql.statement () statement
+      statement = Hasql.Statement q params (Hasql.rowList (hasqlRowDecoder @row)) prepare
+      q = encodeUtf8 (pack neQuery)
+      params = Hasql.noParams
+      prepare = False
+
+
+selectQuery :: forall a . Table Expr a => Query a -> Maybe String
+selectQuery (Query opaleye) = showSqlForPostgresExplicit
+  where
+    showSqlForPostgresExplicit =
+      case Opaleye.runQueryArrUnpack unpackspec opaleye of
+        (x, y, z) -> Opaleye.formatAndShowSQL True (x , Rel8.Optimize.optimize (Opaleye.optimize y) , z)
+
+
+-- | Convert a query to a 'String' containing the query as a @SELECT@
+-- statement.
+showQuery :: Table Expr a => Query a -> String
+showQuery = fold . selectQuery
---
+++ src/Rel8/Table/Opaleye.hs
@@ -0,0 +1,62 @@
+{-# language BlockArguments #-}
+{-# language FlexibleContexts #-}
+{-# language NamedFieldPuns #-}
+{-# language ScopedTypeVariables #-}
+{-# language TypeApplications #-}
+
+module Rel8.Table.Opaleye ( unpackspec, binaryspec, distinctspec, valuesspec ) where
+
+-- rel8
+import qualified Opaleye.Internal.Aggregate as Opaleye
+import qualified Opaleye.Internal.Binary as Opaleye
+import qualified Opaleye.Internal.Distinct as Opaleye
+import qualified Opaleye.Internal.HaskellDB.PrimQuery as Opaleye
+import qualified Opaleye.Internal.PackMap as Opaleye
+import qualified Opaleye.Internal.Unpackspec as Opaleye
+import qualified Opaleye.Internal.Values as Opaleye
+import Rel8.Context ( Context )
+import Rel8.DatabaseType ( DatabaseType( DatabaseType, typeName ) )
+import Rel8.Expr ( Expr, fromPrimExpr, toPrimExpr, traversePrimExpr, unsafeCastExpr )
+import Rel8.HTable ( HTable( HField, hfield, htraverse, htabulate ), hdbtype )
+import Rel8.Table ( Columns, Table( toColumns, fromColumns ) )
+import Rel8.Table.Congruent ( traverseTable, zipTablesWithM )
+
+
+unpackspec :: Table Expr row => Opaleye.Unpackspec row row
+unpackspec =
+  Opaleye.Unpackspec $ Opaleye.PackMap \f ->
+    fmap fromColumns . htraverse (traversePrimExpr f) . addCasts . toColumns
+  where
+    addCasts :: forall f. HTable f => f (Context Expr) -> f (Context Expr)
+    addCasts columns = htabulate go
+      where
+        go :: forall x. HField f x -> Expr x
+        go i = unsafeCastExpr (typeName (hfield hdbtype i)) (hfield columns i)
+
+
+binaryspec :: Table Expr a => Opaleye.Binaryspec a a
+binaryspec =
+  Opaleye.Binaryspec $ Opaleye.PackMap \f (a, b) ->
+    zipTablesWithM (\x y -> fromPrimExpr <$> f (toPrimExpr x, toPrimExpr y)) a b
+
+
+distinctspec :: Table Expr a => Opaleye.Distinctspec a a
+distinctspec =
+  Opaleye.Distinctspec $ Opaleye.Aggregator $ Opaleye.PackMap \f ->
+    traverseTable (\x -> fromPrimExpr <$> f (Nothing, toPrimExpr x))
+
+
+valuesspec :: forall expr. Table Expr expr => Opaleye.Valuesspec expr expr
+valuesspec = Opaleye.ValuesspecSafe packmap unpackspec
+  where
+    packmap :: Opaleye.PackMap Opaleye.PrimExpr Opaleye.PrimExpr () expr
+    packmap = Opaleye.PackMap \f () ->
+      fmap fromColumns $
+        htraverse (traversePrimExpr f) $
+          htabulate @(Columns expr) @Expr \i ->
+            case hfield (hdbtype @(Columns expr)) i of
+              databaseType -> fromPrimExpr $ nullPrimExpr databaseType
+        where
+          nullPrimExpr :: DatabaseType a -> Opaleye.PrimExpr
+          nullPrimExpr DatabaseType{ typeName } =
+            Opaleye.CastExpr typeName (Opaleye.ConstExpr Opaleye.NullLit)
