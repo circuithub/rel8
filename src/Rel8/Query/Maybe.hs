8dc950814580a8baeeb3eac949896aeaa75672b8
Ollie Charles <ollie@ocharles.org.uk>
2021-03-23 16:12:58 +0000

    Document bindMaybeTable and traverseMaybeTable

--- src/Rel8/Query/Maybe.hs
+++ src/Rel8/Query/Maybe.hs
@@ -46,6 +46,21 @@ catMaybeTable ma@(MaybeTable _ a) = do
   pure a
 
 
+-- | @bindMaybeTable f x@ is similar to the monadic bind (@>>=@) operation. It
+-- allows you to "extend" an optional query with another query. If either the
+-- input or output are 'noTable', then the result is 'noTable'.
+--
+-- This is similar to 'traverseMaybeTable', followed by a @join@ on the
+-- resulting @MaybeTable@s.
+--
+-- >>> select c $ bindMaybeTable (optional . values . pure . not_) =<< optional (values [lit True])
+-- [Just False]
+--
+-- >>> select c $ bindMaybeTable (\_ -> return (noTable :: MaybeTable (Expr Bool))) =<< optional (values [lit True])
+-- [Nothing]
+--
+-- >>> select c $ bindMaybeTable (optional . values . pure . not_) =<< return (noTable :: MaybeTable (Expr Bool))
+-- [Nothing]
 bindMaybeTable :: Monad m
   => (a -> m (MaybeTable b)) -> MaybeTable a -> m (MaybeTable b)
 bindMaybeTable query (MaybeTable input a) = do
@@ -53,6 +68,34 @@ bindMaybeTable query (MaybeTable input a) = do
   pure $ MaybeTable (input <> output) b
 
 
+-- | Extend an optional query with another query.  This is useful if you want
+-- to step through multiple @LEFT JOINs@.
+--
+-- Note that @traverseMaybeTable@ takes a @a -> Query b@ function, which means
+-- you also have the ability to "expand" one row into multiple rows.
+--
+-- >>> :{
+-- duplicate :: Expr Bool -> Query (Expr Bool)
+-- duplicate x = unionAll (return x) (return x)
+-- :}
+--
+-- >>> select c $ traverseMaybeTable duplicate =<< optional (values [lit True])
+-- [Just True,Just True]
+--
+-- Note that if the @a -> Query b@ function returns no rows, then the resulting
+-- query will also have no rows:
+--
+-- >>> select c $ traverseMaybeTable (limit 0 . pure) =<< optional (values [lit True])
+-- []
+--
+-- However, regardless of the given @a -> Query b@ function, if the input is
+-- @noTable@, you will always get exactly one @noTable@ back:
+--
+-- >>> select c $ traverseMaybeTable duplicate (noTable :: MaybeTable (Expr Bool))
+-- [Nothing]
+--
+-- >>> select c $ traverseMaybeTable (limit 0 . pure) (noTable :: MaybeTable (Expr Bool))
+-- [Nothing]
 traverseMaybeTable :: (a -> Query b) -> MaybeTable a -> Query (MaybeTable b)
 traverseMaybeTable query ma@(MaybeTable input _) = do
   MaybeTable output b <- optional (query =<< catMaybeTable ma)
