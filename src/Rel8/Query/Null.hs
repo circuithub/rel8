0d21accdc03d99afe0f337ae9202d2b7a9ebc168
Ollie Charles <ollie@ocharles.org.uk>
2021-03-23 16:02:22 +0000

    Document Rel8.Query.Null.catNullable

--- src/Rel8/Query/Null.hs
+++ src/Rel8/Query/Null.hs
@@ -13,6 +13,25 @@ import Rel8.Query ( Query )
 import Rel8.Query.Filter ( where_ )
 
 
+-- | Filter a 'Query' that might return @null@ to a 'Query' without any
+-- @null@s.
+--
+-- Corresponds to 'Data.Maybe.catMaybes'.
+-- 
+-- >>> select c $ pure (nullExpr :: Expr (Maybe Bool))
+-- [Nothing]
+-- 
+-- >>> select c $ catNullable (nullExpr :: Expr (Maybe Bool))
+-- []
+-- 
+-- >>> select c $ catNullable (lit (Just True))
+-- [True]
+-- 
+-- Notice how in the last example a @Bool@ is returned (rather than @Maybe
+-- Bool@):
+-- 
+-- >>> :t catNullable (lit (Just True))
+-- catMaybe (lit (Just True)) :: Query (Expr Bool)
 catNullable :: Expr (Maybe a) -> Query (Expr a)
 catNullable a = do
   where_ $ isNonNull a
