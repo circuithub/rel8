22cbde99fd523c0faddc0552cf94f8f8de32e65f
Ollie Charles <ollie@ocharles.org.uk>
2021-03-24 13:17:59 +0000

    Document Query

--- src/Rel8/Query.hs
+++ src/Rel8/Query.hs
@@ -23,14 +23,18 @@ import Rel8.Table.Alternative
   )
 
 
+-- | The @Query@ monad allows you to compose a @SELECT@ query. This monad has
+-- semantics similar to the list (@[]@) monad.
 type Query :: Type -> Type
 newtype Query a = Query (Opaleye.Select a)
   deriving newtype (Functor, Applicative, Monad)
 
 
+-- | '<|>:' = 'unionAll'.
 instance AltTable Query where
   (<|>:) = unionAll
 
 
+-- | 'emptyTable' = 'values' @[]@.
 instance AlternativeTable Query where
   emptyTable = values []
