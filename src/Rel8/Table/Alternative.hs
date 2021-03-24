0b718acfbbfb0e2589c5be701bb4bd7859a47d15
Ollie Charles <ollie@ocharles.org.uk>
2021-03-24 11:39:19 +0000

    Document Rel8.Table.Alternative

--- src/Rel8/Table/Alternative.hs
+++ src/Rel8/Table/Alternative.hs
@@ -17,12 +17,21 @@ import Rel8.Expr ( Expr )
 import Rel8.Table ( Table )
 
 
+-- | Like 'Alt' in Haskell. This class is purely a Rel8 concept, and allows you
+-- to take a choice between two tables. See also 'AlternativeTable'.
+--
+-- For example, using '<|>:' on 'MaybeTable' allows you to combine two tables
+-- and to return the first one that is a "just" MaybeTable.
 type AltTable :: (Type -> Type) -> Constraint
 class AltTable f where
+  -- | An associative binary operation on 'Table's.
   (<|>:) :: Table Expr a => f a -> f a -> f a
   infixl 3 <|>:
 
 
+-- | Like 'Alternative' in Haskell, some 'Table's form a monoid on applicative
+-- functors.
 type AlternativeTable :: (Type -> Type) -> Constraint
 class AltTable f => AlternativeTable f where
+  -- | The identity of '<|>:'.
   emptyTable :: Table Expr a => f a
