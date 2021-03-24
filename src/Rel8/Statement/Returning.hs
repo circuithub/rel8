d2a6262d927aaa5073ee181f56158c3cad30cf15
Ollie Charles <ollie@ocharles.org.uk>
2021-03-24 13:10:32 +0000

    Document Returning

--- src/Rel8/Statement/Returning.hs
+++ src/Rel8/Statement/Returning.hs
@@ -16,6 +16,11 @@ import Rel8.Table.Recontextualize ( Selects )
 import Rel8.Table.Serialize ( Serializable )
 
 
+-- | @INSERT@, @UPDATE@ and @DELETE@ all support returning either the number of
+-- rows affected, or the actual rows modified. 'Projection' allows you to
+-- project out of these returned rows, which can be useful if you want to log
+-- exactly which rows were deleted, or to view a generated id (for example, if
+-- using a column with an autoincrementing counter as a default value).
 type Returning :: Type -> Type -> Type
 data Returning names a where
   NumberOfRowsAffected :: Returning names Int64
