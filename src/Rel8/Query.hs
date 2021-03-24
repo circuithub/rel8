98fd6f493444c305ced8ca1064aed805e33c9132
Shane O'Brien <shane.obrien@circuithub.com>
2021-03-24 21:01:31 +0000

    Semigroupoids instances for Query 😈

--- src/Rel8/Query.hs
+++ src/Rel8/Query.hs
@@ -1,4 +1,5 @@
 {-# language DerivingStrategies #-}
+{-# language DerivingVia #-}
 {-# language GeneralizedNewtypeDeriving #-}
 {-# language StandaloneKindSignatures #-}
 
@@ -22,12 +23,21 @@ import Rel8.Table.Alternative
   , AlternativeTable, emptyTable
   )
 
+-- semigroupoids
+import Data.Functor.Apply ( Apply, WrappedApplicative(..) )
+import Data.Functor.Bind ( Bind, (>>-) )
+
 
 -- | The @Query@ monad allows you to compose a @SELECT@ query. This monad has
 -- semantics similar to the list (@[]@) monad.
 type Query :: Type -> Type
 newtype Query a = Query (Opaleye.Select a)
   deriving newtype (Functor, Applicative, Monad)
+  deriving Apply via (WrappedApplicative Opaleye.Select)
+
+
+instance Bind Query where
+  (>>-) = (>>=)
 
 
 -- | '<|>:' = 'unionAll'.
