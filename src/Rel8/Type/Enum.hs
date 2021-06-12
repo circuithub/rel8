83466c9f26466c0738917480ab359a3bf14d69cf
Shane O'Brien <shane.obrien@circuithub.com>
2021-06-12 22:57:01 +0100

    Add DBEnum class for specifying enum value names for Enum

--- src/Rel8/Type/Enum.hs
+++ src/Rel8/Type/Enum.hs
@@ -1,27 +1,34 @@
+{-# language AllowAmbiguousTypes #-}
 {-# language DataKinds #-}
 {-# language FlexibleContexts #-}
 {-# language FlexibleInstances #-}
-{-# language GADTs #-}
+{-# language LambdaCase #-}
+{-# language RankNTypes #-}
 {-# language ScopedTypeVariables #-}
 {-# language StandaloneKindSignatures #-}
 {-# language TypeApplications #-}
+{-# language TypeFamilies #-}
 {-# language TypeOperators #-}
 {-# language UndecidableInstances #-}
 
 module Rel8.Type.Enum
-  ( Enum(..)
+  ( Enum( Enum )
+  , DBEnum( enumValue, enumTypeName )
+  , Enumable
   )
 where
 
 -- base
+import Control.Applicative ( (<|>) )
+import Control.Arrow ( (&&&) )
 import Data.Kind ( Constraint, Type )
 import Data.Proxy ( Proxy( Proxy ) )
 import GHC.Generics
   ( Generic, Rep, from, to
-  , (:+:)( L1, R1 ), M1( M1 ), U1( U1 ), Meta( MetaCons )
-  , D, C
+  , (:+:)( L1, R1 ), M1( M1 ), U1( U1 )
+  , D, C, Meta( MetaCons )
   )
-import GHC.TypeLits ( KnownSymbol, Symbol, symbolVal )
+import GHC.TypeLits ( KnownSymbol, symbolVal )
 import Prelude hiding ( Enum )
 
 -- hasql
@@ -32,55 +39,93 @@ import qualified Opaleye.Internal.HaskellDB.PrimQuery as Opaleye
 
 -- rel8
 import Rel8.Type ( DBType, typeInformation )
+import Rel8.Type.Eq ( DBEq )
 import Rel8.Type.Information ( TypeInformation(..) )
+import Rel8.Type.Ord ( DBOrd, DBMax, DBMin )
 
 -- text
-import Data.Text ( unpack )
+import Data.Text ( pack )
 
 
 -- | A deriving-via helper type for column types that store an \"enum\" type
 -- (in Haskell terms, a sum type where all constructors are nullary) using a
 -- Postgres @enum@ type.
 --
--- Note that this must map to a specific type in your database's schema
--- (explicitly created with @CREATE TYPE ... AS ENUM@). The @name@ parameter
--- of @Enum@ should be the name of this type. Note also the names of the
--- Postgres @enum@'s values are assumed to match exactly the names of the
--- constructors of the Haskell type (up to and including case sensitivity).
-type Enum :: Symbol -> Type -> Type
-newtype Enum name a = Enum
+-- Note that this should map to a specific type in your database's schema
+-- (explicitly created with @CREATE TYPE ... AS ENUM@). Use 'DBEnum' to
+-- specify the name of this Postgres type and the names of the individual
+-- values. If left unspecified, the names of the values of the Postgres
+-- @enum@ are assumed to match exactly exactly the names of the constructors
+-- of the Haskell type (up to and including case sensitivity).
+type Enum :: Type -> Type
+newtype Enum a = Enum
   { unEnum :: a
   }
 
 
-instance (KnownSymbol name, Generic a, GEnum (Rep a)) => DBType (Enum name a)
- where
+instance DBEnum a => DBType (Enum a) where
   typeInformation = TypeInformation
-    { decode = Hasql.enum ((`lookup` (fmap (Enum . to) <$> gdecode)) . unpack)
-    , encode = Opaleye.ConstExpr . Opaleye.StringLit . gencode . from . unEnum
-    , typeName = symbolVal (Proxy @name)
+    { decode =
+        Hasql.enum $
+        flip lookup $
+        map ((pack . enumValue &&& Enum) . to) $
+        genumerate @(Rep a)
+    , encode =
+        Opaleye.ConstExpr .
+        Opaleye.StringLit .
+        enumValue @a .
+        unEnum
+    , typeName = enumTypeName @a
     }
 
 
-type GEnum :: (Type -> Type) -> Constraint
-class GEnum rep where
-  gdecode :: [(String, rep x)]
-  gencode :: rep x -> String
+instance DBEnum a => DBEq (Enum a)
 
 
-instance GEnum rep => GEnum (M1 D c rep) where
-  gdecode = fmap M1 <$> gdecode
-  gencode (M1 a) = gencode a
+instance DBEnum a => DBOrd (Enum a)
 
 
-instance (GEnum a, GEnum b) => GEnum (a :+: b) where
-  gdecode = (fmap L1 <$> gdecode) <> (fmap R1 <$> gdecode)
-  gencode (L1 a) = gencode a
-  gencode (R1 a) = gencode a
+instance DBEnum a => DBMax (Enum a)
 
 
-instance (KnownSymbol name, a ~ U1) =>
-  GEnum (M1 C ('MetaCons name f r) a)
+instance DBEnum a => DBMin (Enum a)
+
+
+type DBEnum :: Type -> Constraint
+class (DBType a, Enumable a) => DBEnum a where
+  enumValue :: a -> String
+  enumValue = gshow @(Rep a) . from
+
+  enumTypeName :: String
+
+
+class (Generic a, GEnumable (Rep a)) => Enumable a
+instance (Generic a, GEnumable (Rep a)) => Enumable a
+
+
+type GEnumable :: (Type -> Type) -> Constraint
+class GEnumable rep where
+  genumerate :: [rep x]
+  gshow :: rep x -> String
+
+
+instance GEnumable rep => GEnumable (M1 D meta rep) where
+  genumerate = M1 <$> genumerate
+  gshow (M1 rep) = gshow rep
+
+
+instance (GEnumable a, GEnumable b) => GEnumable (a :+: b) where
+  genumerate = L1 <$> genumerate <|> R1 <$> genumerate
+  gshow = \case
+    L1 a -> gshow a
+    R1 a -> gshow a
+
+
+instance
+  ( meta ~ 'MetaCons name _fixity _isRecord
+  , KnownSymbol name
+  )
+  => GEnumable (M1 C meta U1)
  where
-  gdecode = [(symbolVal (Proxy @name), M1 U1)]
-  gencode _ = symbolVal (Proxy @name)
+  genumerate = [M1 U1]
+  gshow (M1 U1) = symbolVal (Proxy @name)
