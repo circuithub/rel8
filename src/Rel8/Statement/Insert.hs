circuithub/rel8

commit 5d2b9e4cdaa3ebcdaf49a66f39db0913df020e44

Add upsert support and allow arbitrary queries in INSERT, UPDATE and DELETE (#85)
This PR makes several changes to our "manipulation" functions (`insert`, `update`, `delete`).

Firstly, we now support `ON CONFLICT DO UPDATE`, aka "upsert".

Secondly, we now allow the insertion of arbitrary queries (not just static `VALUES`). `values` recovers the old behaviour.

Thirdly, our `Update` and `Delete` statements now support `FROM` and `USING` clauses respectively, allowing joining against other tables.

Fourthly, `Returning` is now an `Applicative`, which means you can say `returning = pure ()` if you don't care about the number of rows affected.

In terms of generating the SQL to implement these features, it was unfortunately significantly less work to roll our own here than to add this upstream to Opaleye proper, because it would have required more refactoring than I felt comfortable doing.

src/Rel8/Statement/Insert.hs
@@ -1,53 +1,53 @@
 {-# language DuplicateRecordFields #-}
+{-# language FlexibleContexts #-}
 {-# language GADTs #-}
 {-# language NamedFieldPuns #-}
-{-# language ScopedTypeVariables #-}
+{-# language RecordWildCards #-}
 {-# language StandaloneKindSignatures #-}
-{-# language TypeApplications #-}
+{-# language StrictData #-}
 
 module Rel8.Statement.Insert
   ( Insert(..)
-  , OnConflict(..)
   , insert
+  , ppInsert
+  , ppInto
   )
 where
 
 -- base
 import Control.Exception ( throwIO )
-import Data.List.NonEmpty ( NonEmpty( (:|) ) )
+import Data.Foldable ( toList )
 import Data.Kind ( Type )
 import Prelude
 
 -- hasql
 import Hasql.Connection ( Connection )
-import qualified Hasql.Decoders as Hasql
 import qualified Hasql.Encoders as Hasql
 import qualified Hasql.Session as Hasql
 import qualified Hasql.Statement as Hasql
 
 -- opaleye
-import qualified Opaleye.Internal.Manipulation as Opaleye
-import qualified Opaleye.Manipulation as Opaleye
+import qualified Opaleye.Internal.HaskellDB.Sql.Print as Opaleye
+
+-- pretty
+import Text.PrettyPrint ( Doc, (<+>), ($$), parens, text )
 
 -- rel8
-import Rel8.Schema.Name ( Selects )
-import Rel8.Schema.Table ( TableSchema )
-import Rel8.Statement.Returning ( Returning( Projection, NumberOfRowsAffected ) )
-import Rel8.Table.Cols ( fromCols, toCols )
-import Rel8.Table.Opaleye ( castTable, table, unpackspec )
-import Rel8.Table.Serialize ( Serializable, parse )
+import Rel8.Query ( Query )
+import Rel8.Schema.Name ( Name, Selects, ppColumn )
+import Rel8.Schema.Table ( TableSchema(..), ppTable )
+import Rel8.Statement.OnConflict ( OnConflict, ppOnConflict )
+import Rel8.Statement.Returning
+  ( Returning
+  , decodeReturning, emptyReturning, ppReturning
+  )
+import Rel8.Statement.Select ( ppSelect )
+import Rel8.Table ( Table )
+import Rel8.Table.Name ( showNames )
 
 -- text
 import qualified Data.Text as Text ( pack )
 import Data.Text.Encoding ( encodeUtf8 )
-
-
--- | @OnConflict@ allows you to add an @ON CONFLICT@ clause to an @INSERT@
--- statement.
-type OnConflict :: Type
-data OnConflict
-  = Abort     -- ^ @ON CONFLICT ABORT@
-  | DoNothing -- ^ @ON CONFLICT DO NOTHING@
 
 
 -- | The constituent parts of a SQL @INSERT@ statement.
@@ -56,9 +56,10 @@ data Insert a where
   Insert :: Selects names exprs =>
     { into :: TableSchema names
       -- ^ Which table to insert into.
-    , rows :: [exprs]
-      -- ^ The rows to insert.
-    , onConflict :: OnConflict
+    , rows :: Query exprs
+      -- ^ The rows to insert. This can be an arbitrary query — use
+      -- 'Rel8.values' insert a static list of rows.
+    , onConflict :: OnConflict names
       -- ^ What to do if the inserted rows conflict with data already in the
       -- table.
     , returning :: Returning names a
@@ -67,52 +68,32 @@ data Insert a where
     -> Insert a
 
 
--- | Run an @INSERT@ statement
+ppInsert :: Insert a -> Maybe Doc
+ppInsert Insert {..} = do
+  rows' <- ppSelect rows
+  pure $ text "INSERT INTO" <+> ppInto into
+    $$ rows'
+    $$ ppOnConflict into onConflict
+    $$ ppReturning into returning
+
+
+ppInto :: Table Name a => TableSchema a -> Doc
+ppInto table@TableSchema {columns} =
+  ppTable table <+>
+  parens (Opaleye.commaV ppColumn (toList (showNames columns)))
+
+
+-- | Run an 'Insert' statement.
 insert :: Connection -> Insert a -> IO a
-insert c Insert {into, rows, onConflict, returning} =
-  case (rows, returning) of
-    ([], NumberOfRowsAffected) -> pure 0
-    ([], Projection _) -> pure []
-
-    (x:xs, NumberOfRowsAffected) -> Hasql.run session c >>= either throwIO pure
+insert connection i@Insert {returning} =
+  case show <$> ppInsert i of
+    Nothing -> pure (emptyReturning returning)
+    Just sql ->
+      Hasql.run session connection >>= either throwIO pure
       where
         session = Hasql.statement () statement
         statement = Hasql.Statement bytes params decode prepare
         bytes = encodeUtf8 $ Text.pack sql
         params = Hasql.noParams
-        decode = Hasql.rowsAffected
+        decode = decodeReturning returning
         prepare = False
-        sql = Opaleye.arrangeInsertManySql into' rows' onConflict'
-          where
-            into' = table $ toCols <$> into
-            rows' = toCols <$> x :| xs
-
-    (x:xs, Projection project) -> Hasql.run session c >>= either throwIO pure
-      where
-        session = Hasql.statement () statement
-        statement = Hasql.Statement bytes params decode prepare
-        bytes = encodeUtf8 $ Text.pack sql
-        params = Hasql.noParams
-        decode = decoder project
-        prepare = False
-        sql =
-          Opaleye.arrangeInsertManyReturningSql
-            unpackspec
-            into'
-            rows'
-            project'
-            onConflict'
-          where
-            into' = table $ toCols <$> into
-            rows' = toCols <$> x :| xs
-            project' = castTable . toCols . project . fromCols
-
-  where
-    onConflict' =
-      case onConflict of
-        DoNothing -> Just Opaleye.DoNothing
-        Abort     -> Nothing
-
-    decoder :: forall exprs projection a. Serializable projection a
-      => (exprs -> projection) -> Hasql.Result [a]
-    decoder _ = Hasql.rowList (parse @projection @a)

src/Rel8/Statement/Select.hs
@@ -1,15 +1,22 @@
+{-# language DeriveTraversable #-}
+{-# language DerivingStrategies #-}
+{-# language FlexibleContexts #-}
 {-# language MonoLocalBinds #-}
 {-# language ScopedTypeVariables #-}
 {-# language TypeApplications #-}
 
 module Rel8.Statement.Select
   ( select
-  , selectWithNames
+  , ppSelect
+
+  , Optimized(..)
+  , ppPrimSelect
   )
 where
 
 -- base
 import Control.Exception ( throwIO )
+import Data.Void ( Void )
 import Prelude
 
 -- hasql
@@ -19,10 +26,26 @@ import qualified Hasql.Encoders as Hasql
 import qualified Hasql.Session as Hasql
 import qualified Hasql.Statement as Hasql
 
+-- opaleye
+import qualified Opaleye.Internal.HaskellDB.Sql as Opaleye
+import qualified Opaleye.Internal.PrimQuery as Opaleye
+import qualified Opaleye.Internal.Print as Opaleye
+import qualified Opaleye.Internal.Optimize as Opaleye
+import qualified Opaleye.Internal.QueryArr as Opaleye hiding ( Select )
+import qualified Opaleye.Internal.Sql as Opaleye
+
+-- pretty
+import Text.PrettyPrint ( Doc )
+
 -- rel8
+import Rel8.Expr ( Expr )
 import Rel8.Query ( Query )
-import Rel8.Query.SQL ( sqlForQuery, sqlForQueryWithNames )
+import Rel8.Query.Opaleye ( toOpaleye )
 import Rel8.Schema.Name ( Selects )
+import Rel8.Table ( Table )
+import Rel8.Table.Cols ( toCols )
+import Rel8.Table.Name ( namesFromLabels )
+import Rel8.Table.Opaleye ( castTable, exprsWithNames )
 import Rel8.Table.Serialize ( Serializable, parse )
 
 -- text
@@ -30,12 +53,12 @@ import qualified Data.Text as Text
 import Data.Text.Encoding ( encodeUtf8 )
 
 
--- | Run a @SELECT@ query, returning all rows.
+-- | Run a @SELECT@ statement, returning all rows.
 select :: forall exprs a. Serializable exprs a
   => Connection -> Query exprs -> IO [a]
-select c query = case sqlForQuery query of
+select c query = case ppSelect query of
   Nothing -> pure []
-  Just sql -> Hasql.run session c >>= either throwIO pure
+  Just doc -> Hasql.run session c >>= either throwIO pure
     where
       session = Hasql.statement () statement
       statement = Hasql.Statement bytes params decode prepare
@@ -43,20 +66,54 @@ select c query = case sqlForQuery query of
       params = Hasql.noParams
       decode = Hasql.rowList (parse @exprs @a)
       prepare = False
+      sql = show doc
 
 
-selectWithNames :: forall exprs a names.
-  ( Selects names exprs
-  , Serializable exprs a
-  )
-  => Connection -> names -> Query exprs -> IO [a]
-selectWithNames c names query = case sqlForQueryWithNames names query of
-  Nothing -> pure []
-  Just sql -> Hasql.run session c >>= either throwIO pure
-    where
-      session = Hasql.statement () statement
-      statement = Hasql.Statement bytes params decode prepare
-      bytes = encodeUtf8 (Text.pack sql)
-      params = Hasql.noParams
-      decode = Hasql.rowList (parse @exprs @a)
-      prepare = False
+ppSelect :: Table Expr a => Query a -> Maybe Doc
+ppSelect query = do
+  primQuery' <- case optimize primQuery of
+    Empty -> Nothing
+    Unit -> Just Opaleye.Unit
+    Optimized primQuery' -> Just primQuery'
+  pure $ Opaleye.ppSql $ primSelectWith names (toCols exprs) primQuery'
+  where
+    names = namesFromLabels
+    (exprs, primQuery, _) =
+      Opaleye.runSimpleQueryArrStart (toOpaleye query) ()
+
+
+ppPrimSelect :: Query a -> (Optimized Doc, a)
+ppPrimSelect query =
+  (Opaleye.ppSql . primSelect <$> optimize primQuery, a)
+  where
+    (a, primQuery, _) =
+      Opaleye.runSimpleQueryArrStart (toOpaleye query) ()
+
+
+data Optimized a = Empty | Unit | Optimized a
+  deriving stock (Functor, Foldable, Traversable)
+
+
+optimize :: Opaleye.PrimQuery' a -> Optimized (Opaleye.PrimQuery' Void)
+optimize query = case Opaleye.removeEmpty (Opaleye.optimize query) of
+  Nothing -> Empty
+  Just Opaleye.Unit -> Unit
+  Just query' -> Optimized query'
+
+
+primSelect :: Opaleye.PrimQuery' Void -> Opaleye.Select
+primSelect = Opaleye.foldPrimQuery Opaleye.sqlQueryGenerator
+
+
+primSelectWith :: Selects names exprs
+  => names -> exprs -> Opaleye.PrimQuery' Void -> Opaleye.Select
+primSelectWith names exprs query =
+  Opaleye.SelectFrom $ Opaleye.newSelect
+    { Opaleye.attrs = Opaleye.SelectAttrs attrs
+    , Opaleye.tables = Opaleye.oneTable (primSelect query)
+    }
+  where
+    attrs = makeAttr <$> exprsWithNames names (castTable exprs)
+      where
+        makeAttr (label, expr) =
+          (Opaleye.sqlExpr expr, Just (Opaleye.SqlColumn label))

rel8.cabal
@@ -27,6 +27,7 @@ library
     , contravariant
     , hasql ^>= 1.4.5.1
     , opaleye ^>= 0.7.3.0
+    , pretty
     , profunctors
     , scientific
     , semialign
@@ -138,10 +139,15 @@ library
 
     Rel8.Statement.Delete
     Rel8.Statement.Insert
+    Rel8.Statement.OnConflict
     Rel8.Statement.Returning
     Rel8.Statement.Select
+    Rel8.Statement.Set
+    Rel8.Statement.SQL
     Rel8.Statement.Update
+    Rel8.Statement.Using
     Rel8.Statement.View
+    Rel8.Statement.Where
 
     Rel8.Table
     Rel8.Table.ADT

src/Rel8.hs
@@ -244,6 +244,9 @@ module Rel8
   , nullsFirst
   , nullsLast
 
+    -- ** Bindings
+  , rebind
+
     -- * IO
   , Serializable
   , ToExprs
@@ -258,16 +261,20 @@ module Rel8
     -- ** @INSERT@
   , Insert(..)
   , OnConflict(..)
+  , Upsert(..)
   , insert
   , unsafeDefault
+  , showInsert
 
     -- ** @DELETE@
   , Delete(..)
   , delete
+  , showDelete
 
     -- ** @UPDATE@
   , Update(..)
   , update
+  , showUpdate
 
     -- ** @.. RETURNING@
   , Returning(..)
@@ -332,8 +339,10 @@ import Rel8.Schema.Result ( Result )
 import Rel8.Schema.Table
 import Rel8.Statement.Delete
 import Rel8.Statement.Insert
+import Rel8.Statement.OnConflict
 import Rel8.Statement.Returning
 import Rel8.Statement.Select
+import Rel8.Statement.SQL
 import Rel8.Statement.Update
 import Rel8.Statement.View
 import Rel8.Table

src/Rel8/Query/SQL.hs
@@ -1,75 +1,21 @@
 {-# language FlexibleContexts #-}
-{-# language TypeFamilies #-}
-{-# language ViewPatterns #-}
+{-# language MonoLocalBinds #-}
 
 module Rel8.Query.SQL
   ( showQuery
-  , sqlForQuery, sqlForQueryWithNames
   )
 where
 
 -- base
-import Data.Foldable ( fold )
-import Data.Functor.Const ( Const( Const ), getConst )
-import Data.Void ( Void )
 import Prelude
-
--- opaleye
-import qualified Opaleye.Internal.HaskellDB.Sql as Opaleye
-import qualified Opaleye.Internal.PrimQuery as Opaleye
-import qualified Opaleye.Internal.Print as Opaleye
-import qualified Opaleye.Internal.Optimize as Opaleye
-import qualified Opaleye.Internal.QueryArr as Opaleye hiding ( Select )
-import qualified Opaleye.Internal.Sql as Opaleye
 
 -- rel8
 import Rel8.Expr ( Expr )
-import Rel8.Expr.Opaleye ( toPrimExpr )
 import Rel8.Query ( Query )
-import Rel8.Query.Opaleye ( toOpaleye )
-import Rel8.Schema.Name ( Name( Name ), Selects )
-import Rel8.Schema.HTable ( htabulateA, hfield )
-import Rel8.Table ( Table, toColumns )
-import Rel8.Table.Cols ( toCols )
-import Rel8.Table.Name ( namesFromLabels )
-import Rel8.Table.Opaleye ( castTable )
+import Rel8.Statement.Select ( ppSelect )
+import Rel8.Table ( Table )
 
 
--- | Convert a query to a 'String' containing the query as a @SELECT@
--- statement.
+-- | Convert a 'Query' to a 'String' containing a @SELECT@ statement.
 showQuery :: Table Expr a => Query a -> String
-showQuery = fold . sqlForQuery
-
-
-sqlForQuery :: Table Expr a
-  => Query a -> Maybe String
-sqlForQuery = sqlForQueryWithNames namesFromLabels . fmap toCols
-
-
-sqlForQueryWithNames :: Selects names exprs
-  => names -> Query exprs -> Maybe String
-sqlForQueryWithNames names query =
-  show . Opaleye.ppSql . selectFrom names exprs <$> optimize primQuery
-  where
-    (exprs, primQuery, _) =
-      Opaleye.runSimpleQueryArrStart (toOpaleye query) ()
-
-
-optimize :: Opaleye.PrimQuery' a -> Maybe (Opaleye.PrimQuery' Void)
-optimize = Opaleye.removeEmpty . Opaleye.optimize
-
-
-selectFrom :: Selects names exprs
-  => names -> exprs -> Opaleye.PrimQuery' Void -> Opaleye.Select
-selectFrom (toColumns -> names) (toColumns . castTable -> exprs) query =
-  Opaleye.SelectFrom $ Opaleye.newSelect
-    { Opaleye.attrs = Opaleye.SelectAttrs attributes
-    , Opaleye.tables = Opaleye.oneTable select
-    }
-  where
-    select = Opaleye.foldPrimQuery Opaleye.sqlQueryGenerator query
-    attributes = getConst $ htabulateA $ \field -> case hfield names field of
-      Name name -> case hfield exprs field of
-        expr -> Const (pure (makeAttr name (toPrimExpr expr)))
-    makeAttr label expr =
-      (Opaleye.sqlExpr expr, Just (Opaleye.SqlColumn label))
+showQuery = foldMap show . ppSelect

src/Rel8/Schema/Name.hs
@@ -13,6 +13,7 @@
 module Rel8.Schema.Name
   ( Name(..)
   , Selects
+  , ppColumn
   )
 where
 
@@ -21,6 +22,13 @@ import Data.Functor.Identity ( Identity( Identity ) )
 import Data.Kind ( Constraint, Type )
 import Data.String ( IsString )
 import Prelude
+
+-- opaleye
+import qualified Opaleye.Internal.HaskellDB.Sql as Opaleye
+import qualified Opaleye.Internal.HaskellDB.Sql.Print as Opaleye
+
+-- pretty
+import Text.PrettyPrint ( Doc )
 
 -- rel8
 import Rel8.Expr ( Expr )
@@ -63,3 +71,7 @@ instance Sql DBType a => Table Name (Name a) where
 type Selects :: Type -> Type -> Constraint
 class Transposes Name Expr names exprs => Selects names exprs
 instance Transposes Name Expr names exprs => Selects names exprs
+
+
+ppColumn :: String -> Doc
+ppColumn = Opaleye.ppSqlExpr . Opaleye.ColumnSqlExpr . Opaleye.SqlColumn

src/Rel8/Schema/Table.hs
@@ -1,13 +1,23 @@
 {-# language DeriveFunctor #-}
 {-# language DerivingStrategies #-}
+{-# language DisambiguateRecordFields #-}
+{-# language NamedFieldPuns #-}
 
 module Rel8.Schema.Table
   ( TableSchema(..)
+  , ppTable
   )
 where
 
 -- base
 import Prelude
+
+-- opaleye
+import qualified Opaleye.Internal.HaskellDB.Sql as Opaleye
+import qualified Opaleye.Internal.HaskellDB.Sql.Print as Opaleye
+
+-- pretty
+import Text.PrettyPrint ( Doc )
 
 
 -- | The schema for a table. This is used to specify the name and schema that a
@@ -27,3 +37,10 @@ data TableSchema names = TableSchema
     -- data type here, parameterized by the 'Rel8.ColumnSchema.ColumnSchema' functor.
   }
   deriving stock Functor
+
+
+ppTable :: TableSchema a -> Doc
+ppTable TableSchema {name, schema} = Opaleye.ppTable Opaleye.SqlTable
+  { sqlTableSchemaName = schema
+  , sqlTableName = name
+  }

src/Rel8/Statement/Delete.hs
@@ -1,13 +1,15 @@
 {-# language DuplicateRecordFields #-}
 {-# language GADTs #-}
 {-# language NamedFieldPuns #-}
-{-# language ScopedTypeVariables #-}
+{-# language RankNTypes #-}
+{-# language RecordWildCards #-}
 {-# language StandaloneKindSignatures #-}
-{-# language TypeApplications #-}
+{-# language StrictData #-}
 
 module Rel8.Statement.Delete
   ( Delete(..)
   , delete
+  , ppDelete
   )
 where
 
@@ -18,23 +20,24 @@ import Prelude
 
 -- hasql
 import Hasql.Connection ( Connection )
-import qualified Hasql.Decoders as Hasql
 import qualified Hasql.Encoders as Hasql
 import qualified Hasql.Session as Hasql
 import qualified Hasql.Statement as Hasql
 
--- opaleye
-import qualified Opaleye.Internal.Manipulation as Opaleye
+-- pretty
+import Text.PrettyPrint ( Doc, (<+>), ($$), text )
 
 -- rel8
 import Rel8.Expr ( Expr )
-import Rel8.Expr.Opaleye ( toColumn, toPrimExpr )
+import Rel8.Query ( Query )
 import Rel8.Schema.Name ( Selects )
-import Rel8.Schema.Table ( TableSchema )
-import Rel8.Statement.Returning ( Returning( NumberOfRowsAffected, Projection ) )
-import Rel8.Table.Cols ( fromCols, toCols )
-import Rel8.Table.Opaleye ( castTable, table, unpackspec )
-import Rel8.Table.Serialize ( Serializable, parse )
+import Rel8.Schema.Table ( TableSchema, ppTable )
+import Rel8.Statement.Returning
+  ( Returning
+  , decodeReturning, emptyReturning, ppReturning
+  )
+import Rel8.Statement.Using ( ppUsing )
+import Rel8.Statement.Where ( ppWhere )
 
 -- text
 import qualified Data.Text as Text
@@ -47,7 +50,10 @@ data Delete a where
   Delete :: Selects names exprs =>
     { from :: TableSchema names
       -- ^ Which table to delete from.
-    , deleteWhere :: exprs -> Expr Bool
+    , using :: Query using
+      -- ^ @USING@ clause — this can be used to join against other tables,
+      -- and its results can be referenced in the @WHERE@ clause
+    , deleteWhere :: using -> exprs -> Expr Bool
       -- ^ Which rows should be selected for deletion.
     , returning :: Returning names a
       -- ^ What to return from the @DELETE@ statement.
@@ -55,38 +61,26 @@ data Delete a where
     -> Delete a
 
 
--- | Run a @DELETE@ statement.
-delete :: Connection -> Delete a -> IO a
-delete c Delete {from, deleteWhere, returning} =
-  case returning of
-    NumberOfRowsAffected -> Hasql.run session c >>= either throwIO pure
-      where
-        session = Hasql.statement () statement
-        statement = Hasql.Statement bytes params decode prepare
-        bytes = encodeUtf8 $ Text.pack sql
-        params = Hasql.noParams
-        decode = Hasql.rowsAffected
-        prepare = False
-        sql = Opaleye.arrangeDeleteSql from' where'
-          where
-            from' = table $ toCols <$> from
-            where' = toColumn . toPrimExpr . deleteWhere . fromCols
+ppDelete :: Delete a -> Maybe Doc
+ppDelete Delete {..} = do
+  (usingDoc, i) <- ppUsing using
+  pure $ text "DELETE FROM" <+> ppTable from
+    $$ usingDoc
+    $$ ppWhere from (deleteWhere i)
+    $$ ppReturning from returning
 
-    Projection project -> Hasql.run session c >>= either throwIO pure
+
+-- | Run a 'Delete' statement.
+delete :: Connection -> Delete a -> IO a
+delete connection d@Delete {returning} =
+  case show <$> ppDelete d of
+    Nothing -> pure (emptyReturning returning)
+    Just sql ->
+      Hasql.run session connection >>= either throwIO pure
       where
         session = Hasql.statement () statement
         statement = Hasql.Statement bytes params decode prepare
         bytes = encodeUtf8 $ Text.pack sql
         params = Hasql.noParams
-        decode = decoder project
+        decode = decodeReturning returning
         prepare = False
-        sql =
-          Opaleye.arrangeDeleteReturningSql unpackspec from' where' project'
-          where
-            from' = table $ toCols <$> from
-            where' = toColumn . toPrimExpr . deleteWhere . fromCols
-            project' = castTable . toCols . project . fromCols
-  where
-    decoder :: forall exprs projection a. Serializable projection a
-      => (exprs -> projection) -> Hasql.Result [a]
-    decoder _ = Hasql.rowList (parse @projection @a)

commit 1db869d32f291013a829149a90d83b14e12e177b

Fix insertion of default values since broken by #85
When printing the SQL, we analyse the `PrimExpr`s and `PrimQuery` to detect the case where `rows` is set to `rows = values _` and, if so, we output a bare `VALUES` statement instead of a `SELECT`, because `DEFAULT` is only syntactically valid in a bare `VALUES` clause.

src/Rel8/Statement/Insert.hs
@@ -35,7 +35,7 @@ import Rel8.Schema.Name ( Name, Selects, ppColumn )
 import Rel8.Schema.Table ( TableSchema(..), ppTable )
 import Rel8.Statement.OnConflict ( OnConflict, ppOnConflict )
 import Rel8.Statement.Returning ( Returning, decodeReturning, ppReturning )
-import Rel8.Statement.Select ( ppSelect )
+import Rel8.Statement.Select ( ppRows )
 import Rel8.Table ( Table )
 import Rel8.Table.Name ( showNames )
 
@@ -66,7 +66,7 @@ ppInsert :: Insert a -> Doc
 ppInsert Insert {..} =
   text "INSERT INTO" <+>
   ppInto into $$
-  ppSelect rows $$
+  ppRows rows $$
   ppOnConflict into onConflict $$
   ppReturning into returning
 

src/Rel8/Statement/Select.hs
@@ -11,10 +11,13 @@ module Rel8.Statement.Select
 
   , Optimized(..)
   , ppPrimSelect
+  , ppRows
   )
 where
 
 -- base
+import Data.Foldable ( toList )
+import Data.List.NonEmpty ( NonEmpty( (:|) ) )
 import Data.Void ( Void )
 import Prelude hiding ( undefined )
 
@@ -24,12 +27,15 @@ import qualified Hasql.Encoders as Hasql
 import qualified Hasql.Statement as Hasql
 
 -- opaleye
+import qualified Opaleye.Internal.HaskellDB.PrimQuery as Opaleye
 import qualified Opaleye.Internal.HaskellDB.Sql as Opaleye
+import qualified Opaleye.Internal.HaskellDB.Sql.Print as Opaleye
 import qualified Opaleye.Internal.PrimQuery as Opaleye
 import qualified Opaleye.Internal.Print as Opaleye
 import qualified Opaleye.Internal.Optimize as Opaleye
 import qualified Opaleye.Internal.QueryArr as Opaleye hiding ( Select )
-import qualified Opaleye.Internal.Sql as Opaleye
+import qualified Opaleye.Internal.Sql as Opaleye hiding ( Values )
+import qualified Opaleye.Internal.Tag as Opaleye
 
 -- pretty
 import Text.PrettyPrint ( Doc )
@@ -45,6 +51,7 @@ import Rel8.Table ( Table )
 import Rel8.Table.Cols ( toCols )
 import Rel8.Table.Name ( namesFromLabels )
 import Rel8.Table.Opaleye ( castTable, exprsWithNames )
+import qualified Rel8.Table.Opaleye as T
 import Rel8.Table.Serialize ( Serializable, parse )
 import Rel8.Table.Undefined ( undefined )
 
@@ -80,12 +87,33 @@ ppSelect query =
     never = pure (toPrimExpr false)
 
 
+ppRows :: Table Expr a => Query a -> Doc
+ppRows query = case optimize primQuery of
+  -- Special case VALUES because we can't use DEFAULT inside a SELECT
+  Optimized (Opaleye.Product ((_, Opaleye.Values symbols rows) :| []) [])
+    | eqSymbols symbols (toList (T.exprs a)) ->
+        Opaleye.ppValues_ (map Opaleye.sqlExpr <$> toList rows)
+  _ -> ppSelect query
+  where
+    (a, primQuery, _) = Opaleye.runSimpleQueryArrStart (toOpaleye query) ()
+
+    eqSymbols (symbol : symbols) (Opaleye.AttrExpr symbol' : exprs)
+      | eqSymbol symbol symbol' = eqSymbols symbols exprs
+      | otherwise = False
+    eqSymbols [] [] = True
+    eqSymbols _ _ = False
+
+    eqSymbol
+      (Opaleye.Symbol name (Opaleye.UnsafeTag tag))
+      (Opaleye.Symbol name' (Opaleye.UnsafeTag tag'))
+      = name == name' && tag == tag'
+
+
 ppPrimSelect :: Query a -> (Optimized Doc, a)
 ppPrimSelect query =
   (Opaleye.ppSql . primSelect <$> optimize primQuery, a)
   where
-    (a, primQuery, _) =
-      Opaleye.runSimpleQueryArrStart (toOpaleye query) ()
+    (a, primQuery, _) = Opaleye.runSimpleQueryArrStart (toOpaleye query) ()
 
 
 data Optimized a = Empty | Unit | Optimized a