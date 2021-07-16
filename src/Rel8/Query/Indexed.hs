c4bba4eb93afe40bb3dc4f5d2dc59f1bc222bfb2
Shane O'Brien <shane.obrien@circuithub.com>
2021-07-16 15:42:30 +0100

    Add indexed combinator

---
+++ src/Rel8/Query/Indexed.hs
@@ -0,0 +1,34 @@
+module Rel8.Query.Indexed
+  ( indexed
+  )
+where
+
+-- base
+import Data.Int ( Int64 )
+import Prelude
+
+-- opaleye
+import qualified Opaleye.Internal.HaskellDB.PrimQuery as Opaleye
+import qualified Opaleye.Internal.PackMap as Opaleye
+import qualified Opaleye.Internal.PrimQuery as Opaleye
+import qualified Opaleye.Internal.QueryArr as Opaleye
+import qualified Opaleye.Internal.Tag as Opaleye
+
+-- rel8
+import Rel8.Expr ( Expr )
+import Rel8.Expr.Opaleye ( fromPrimExpr )
+import Rel8.Query ( Query )
+import Rel8.Query.Opaleye ( mapOpaleye )
+
+
+-- | Pair each row of a query with its index within the query.
+indexed :: Query a -> Query (Expr Int64, a)
+indexed = mapOpaleye $ \(Opaleye.QueryArr f) -> Opaleye.QueryArr $ \(_, tag) ->
+  let
+    (a, query, tag') = f ((), tag)
+    tag'' = Opaleye.next tag'
+    window = Opaleye.ConstExpr $ Opaleye.OtherLit "ROW_NUMBER() OVER () - 1"
+    (index, bindings) = Opaleye.run $ Opaleye.extractAttr "index" tag' window
+    query' lateral = Opaleye.Rebind True bindings . query lateral
+  in
+    ((fromPrimExpr index, a), query', tag'')
