1af8a1791a788038a9a487bd58cab3db1ea6a7d4
Tom Ellis <tom-git@jaguarpaw.co.uk>
2022-02-12 13:36:36 +0000

    Use rebindExplicitPrefix

--- src/Rel8/Query/Rebind.hs
+++ src/Rel8/Query/Rebind.hs
@@ -7,29 +7,21 @@ where
 
 -- base
 import Prelude
+import Control.Arrow ((<<<))
 
 -- opaleye
-import qualified Opaleye.Internal.PackMap as Opaleye
-import qualified Opaleye.Internal.PrimQuery as Opaleye
-import qualified Opaleye.Internal.QueryArr as Opaleye
-import qualified Opaleye.Internal.Tag as Opaleye
-import qualified Opaleye.Internal.Unpackspec as Opaleye
+import qualified Opaleye.Internal.Rebind as Opaleye
 
 -- rel8
 import Rel8.Expr ( Expr )
-import Rel8.Query ( Query( Query ) )
+import Rel8.Query ( Query )
 import Rel8.Table ( Table )
 import Rel8.Table.Opaleye ( unpackspec )
+import Rel8.Query.Opaleye (fromOpaleye)
 
 
 -- | 'rebind' takes a variable name, some expressions, and binds each of them
 -- to a new variable in the SQL. The @a@ returned consists only of these
 -- variables. It's essentially a @let@ binding for Postgres expressions.
 rebind :: Table Expr a => String -> a -> Query a
-rebind prefix a = Query $ \_ -> Opaleye.QueryArr $ \(_, tag) ->
-  let
-    tag' = Opaleye.next tag
-    (a', bindings) = Opaleye.run $
-      Opaleye.runUnpackspec unpackspec (Opaleye.extractAttr prefix tag) a
-  in
-    ((mempty, a'), \_ -> Opaleye.Rebind True bindings, tag')
+rebind prefix a = fromOpaleye (Opaleye.rebindExplicitPrefix prefix unpackspec <<< pure a)
