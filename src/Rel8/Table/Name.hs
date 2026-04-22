555c3f967223743e502f94733f13951f1161fec0
Cristóvão Gomes Ferreira <cristovao.ferreira@artificial.io>
2026-04-22 16:30:59 +0100

    Truncate large names, with a tag to avoid collisions

--- src/Rel8/Table/Name.hs
+++ src/Rel8/Table/Name.hs
@@ -11,10 +11,12 @@
 
 module Rel8.Table.Name
   ( namesFromLabels
+  , namesFromLabelsTagged
   , namesFromLabelsWith
   , namesFromLabelsWithA
   , showLabels
   , showNames
+  , shortenName
   )
 where
 
@@ -26,6 +28,9 @@ import Data.List.NonEmpty ( NonEmpty, intersperse, nonEmpty )
 import Data.Maybe ( fromMaybe )
 import Prelude
 
+-- opaleye
+import qualified Opaleye.Internal.Tag as Opaleye
+
 -- rel8
 import Rel8.Schema.HTable (htabulateA, hfield, hspecs)
 import Rel8.Schema.Name ( Name( Name ) )
@@ -35,15 +40,41 @@ import Rel8.Table ( Table(..) )
 -- semigroupoids
 import Data.Functor.Apply (Apply)
 
+-- transformers
+import Control.Monad.Trans.State.Strict (State, evalState)
+
 
 -- | Construct a table in the 'Name' context containing the names of all
--- columns. Nested column names will be combined with @/@.
+-- columns. Nested column names will be combined with @/@, the resulting
+-- name will be truncated and a unique tag appended to the end of the name
+-- so that the resulting name has 63 or less characters (Postgres' default
+-- maximum column name length).
 --
--- See also: 'namesFromLabelsWith'.
+-- See also: 'namesFromLabelsTagged', 'namesFromLabelsWith'.
 namesFromLabels :: Table Name a => a
-namesFromLabels = namesFromLabelsWith go
+namesFromLabels = namesFromLabelsWithA (shortenName Nothing) `evalState` Opaleye.start
+
+
+-- | Similar to 'namesFromLabels', but receives an additional 'Opaleye.Tag'
+-- to distinguish between relations. Resulting names will also have 63 or
+-- less characters.
+namesFromLabelsTagged :: Table Name a => Opaleye.Tag -> a
+namesFromLabelsTagged relationTag = namesFromLabelsWithA (shortenName (Just relationTag)) `evalState` Opaleye.start
+
+
+-- | Map a non-empty list of labels to a short SQL identifier with an opaleye tag appended,
+-- truncated if it would be too large.
+shortenName :: Maybe Opaleye.Tag -> NonEmpty String -> State Opaleye.Tag String
+shortenName mtag labels = do
+  subtag <- Opaleye.fresh
+  let
+    addRelationTag = case mtag of
+      Nothing -> id
+      Just tag -> Opaleye.tagWith tag
+    suffix = addRelationTag (Opaleye.tagWith subtag "")
+  pure $ take (63 - length suffix) label ++ suffix
   where
-    go = fold . intersperse "/"
+    label = fold (intersperse "/" labels)
 
 
 -- | Construct a table in the 'Name' context containing the names of all
